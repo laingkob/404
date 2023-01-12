667a3d7cee75b8d7c2494ea7291d0809469b8698
Courtenay Laing-Kobe <laingkob@ualberta.ca>
2023-01-11 19:40:12 -0700

    modified

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://webdocs.cs.ualberta.ca/~hindle1/1.py"
+url = "https://raw.githubusercontent.com/laingkob/404/lab1/script.py"
 r = requests.get(url, allow_redirects=True)
 print(r.text)
 
